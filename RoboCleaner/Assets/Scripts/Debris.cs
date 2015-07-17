bda3ca4a60755dc81ae4c19f1b03a4ac2153a276
DeeCeptor <sirmichaellong@gmail.com>
2015-07-17 13:29:43 -0600

    no message

--- RoboCleaner/Assets/Scripts/Debris.cs
+++ RoboCleaner/Assets/Scripts/Debris.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Debris : MonoBehaviour {
+public class Debris : MonoBehaviour 
+{
+	private bool beingPulled = false;
+	private GameObject pulledTowards;
+	private float pullSpeedFactor = 1.0f;
+
 
 	void Start () 
 	{
@@ -10,15 +15,27 @@ public class Debris : MonoBehaviour {
 	
 	void Update () 
 	{
-	
+		if (beingPulled)
+		{
+			// Move towards player, the closer they are the more quickly
+			Vector3 diff = this.transform.position - pulledTowards.transform.position;
+			pulledTowards.transform.position += diff / diff.magnitude * pullSpeedFactor;
+		}
 	}
 
 
-	void OnTriggerStay2D(Collider2D other) {
+	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "TractorBeam")
 		{
-
+			// Entered radius of player, get pulled in
+			beingPulled = true;
+			pulledTowards = other.gameObject;
 		}
-		other.attachedRigidbody.AddForce(-0.1F * other.attachedRigidbody.velocity);
+		else if (other.tag == "Player")
+		{
+			// Touchwed player's inner collider, collect points and destroy this!
+			Debug.Log("POINTS!");
+			GameObject.Destroy(this);
+		}
 	}
 }
