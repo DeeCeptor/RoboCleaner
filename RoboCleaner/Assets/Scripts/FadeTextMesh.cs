77b9e254dafe1b90fb6872dc3eead66c80e99787
DeeCeptor <sirmichaellong@gmail.com>
2015-07-17 23:23:18 -0600

    Added score text on debris pickup

---
+++ RoboCleaner/Assets/Scripts/FadeTextMesh.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeTextMesh : MonoBehaviour 
+{
+	private float alpha = 1;
+	private float fadingOverTime = 3.0f;
+	private Color curColor;
+
+	void Start () 
+	{
+		curColor = transform.GetComponent<TextMesh>().color;
+	}
+	
+	void Update () 
+	{
+		alpha -= Mathf.Clamp01(Time.deltaTime / fadingOverTime);
+		curColor.a = alpha;
+		transform.GetComponent<TextMesh>().color = curColor;
+	}
+}
