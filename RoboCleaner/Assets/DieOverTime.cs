e8185885cee3f7d4d8ef41a08357310ea8e29881
Squirrell <ksquires11@gmail.com>
2015-07-17 20:38:44 -0800

    Lasers!

---
+++ RoboCleaner/Assets/DieOverTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieOverTime : MonoBehaviour {
+public float timerToDie = 0;
+public float LifeSpan = 4;
+	// Use this for initialization
+	void Start () {
+	timerToDie = Time.time + LifeSpan;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	if(timerToDie <= Time.time)
+	{
+	Destroy(gameObject);
+	}
+	}
+}
---
+++ RoboCleaner/Assets/TransformIntoLaser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserOverTime : MonoBehaviour {
+public float delay = 4f;
+public float timeToLaser = 0;
+public Transform Laser;
+	// Use this for initialization
+	void Start () {
+	timeToLaser = Time.time + delay;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	if(timeToLaser <= Time.time)
+	{
+	Instantiate(Laser,transform.position,transform.rotation);
+	Destroy(gameObject);
+	}
+	}
+}
