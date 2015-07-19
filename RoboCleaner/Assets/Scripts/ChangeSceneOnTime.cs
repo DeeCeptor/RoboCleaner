cf1fb10b5aecd444334fbf22addddd036808c916
Squirrell <ksquires11@gmail.com>
2015-07-19 11:49:28 -0800

    intro and difficulty changes

---
+++ RoboCleaner/Assets/Scripts/ChangeSceneOnTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneOnTime : MonoBehaviour {
+	public float timerToChange = 0;
+	public float ChangeTime = 15f;
+	// Use this for initialization
+	void Start () 
+	{
+		timerToChange = Time.time + ChangeTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(timerToChange <= Time.time)
+		{
+			Application.LoadLevel("KevinLevel");
+		}
+	}
+}
