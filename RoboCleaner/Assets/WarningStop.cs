4bf46d3441a8d29d5522dfe03d2c90f7f67f3cf1
Squirrell <ksquires11@gmail.com>
2015-07-19 11:04:21 -0800

    Warning Lines

---
+++ RoboCleaner/Assets/WarningStop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarningStop : MonoBehaviour {
+	public float stopTime = 4f;
+	public float warpTime = 8f;
+	private float stopTimer;
+	// Use this for initialization
+	void Start () {
+	stopTimer = Time.time + stopTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	if(stopTimer<Time.time)
+	{
+	GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+	AIZoomba zoomScript = GetComponent<AIZoomba>();
+	GetComponent<AudioSource>().mute = true;
+	zoomScript.accelerationForce = 0;
+			zoomScript.engineEmitter.GetComponent<ParticleSystem>().Stop();
+			zoomScript.smokeEmitter.GetComponent<ParticleSystem>().Stop();
+			zoomScript.heatEmitter.GetComponent<ParticleSystem>().Stop();
+	}
+	
+	}
+}
